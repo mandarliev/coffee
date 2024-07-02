3b611b5166f37f01e188c1decfc8a2bea017bccc
Dimitar <dimitar.mandarliev@gmail.com>
2024-07-02 21:11:49 +0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
